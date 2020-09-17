369dca10966c5cee5b8d3ccb40f14401f6594e18
raunak singh <68766724+Raunaks068619@users.noreply.github.com>
2020-09-17 17:53:34 +0530

    Update home.php

--- home with login/home.php
+++ home with login/home.php
@@ -8,14 +8,13 @@ include('login.php');
 <head>
   <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
-<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link href="css/business-frontpage.css" rel="stylesheet">
--->
+
 <link rel="stylesheet" href="home.css?v=<?php echo time(); ?>">
 </head>
 <body>
-<!--header-->
 
   <!-- Navigation area-->
 <div class="logo">
@@ -23,58 +22,64 @@ include('login.php');
 </div>
   
 <div class="topnav" id="myTopnav"> 
-    <a href="home.php" class="active">Home</a>
-    <a href="C:\Users\rauna\Desktop\ip\Webdev\Webdev.html">WebDev</a>
-      <div class="dropdown">
-        <button onclick="" class="dropbtn">WebForum</button> 
-      </div> 
+    <a href="#" class="active">Home</a>
+    <a href="webdev.php">WebDev</a>
+    <a href="#">WebForum</a>
     <a href="#about">About</a>
-   <?php if (!isset($_SESSION["uname"])): ?>
-	<a class="login" onclick="document.getElementById('login').style.display='block'" style="width:auto;">Login</a>
-	
+    <?php if (!isset($_SESSION["uname"])): ?>
+	<a class="reg" onclick="document.getElementById('login').style.display='block'" style="width:auto;">Login</a>
+
 	<?php else : ?>
 	
-	<a class="login"><?php echo 'Welcome ';echo $name ?></a>
+	<a class="reg"><?php echo 'Welcome ';echo $name ?></a>
 	<a  href="logout.php">Logout </a>
-	<?php endif; ?><br>
+	<?php endif; ?>
     <a href="javascript:void(0);" style="font-size: 20px;" class="icon" onclick="myFunction()">☰</a>
 </div>
 
-<div class="banner">
-  <div class="apptext">
-      <h1>Learn Web Developement &amp; Communicate using Forum</h1>
-      <p>Learn Wedpage developement from Basic level to intermidiate level
-      <br>Exploring HTML - CSS - JavaScript in simplest ways, 
-      <br>Including examples and online Text Editor to practice your Skills 
-      <br>Use our Forum to Ask &amp;Solve Question asked by students
-      </p>
-  </div>
-  <div class="picture">
-      <img src="_Pngtree_web_development_illustration_modern_4461019-removebg-preview.png" alt="">
-  </div>
-</div>
-
-<div class="text-resp">
-  <h2>NoteScout</h2>
-  <h1>Master Web Developement/Design<br>
-  with us</h1>
-  <p>Learn Wedpage developement from Intermediate level to Advanced level</p>
+<div class="container">
+  <div class="row">
+    <div class="column-66">
+      <div style="color: aliceblue;">
+        <h1>
+        Learn web
+        <br>Development &amp;
+        <br>Communicate Using
+        <br>Forum</h1>
+    <p style="font-size: 17px;">
+    Learn Wedpage developement from Intermidiate level to Advanced level
     <img src="Capture-removebg-preview.png" alt="web">
-    <div><p>Explore HTML - CSS - JavaScript in esiest manner, 
-    Including examples and online Text Editor to practice your Skills, 
-    Use our Forum to Ask &amp;Solve Question asked by students
-    </p></div>
-    <img src="qw.png" alt="">
+Exploring HTML - CSS - JavaScript in simplest ways,
+Including examples and online Text Editor to practice your Skills
+Use our Forum to Ask &amp;Solve Question asked by students
+<img src="qw.png" alt="web"></p></div></div>
+
+  <div class="column-33">
+      <img class="img1" src="_Pngtree_web_development_illustration_modern_4461019-removebg-preview.png" width="335" height="471">
+  </div>
+  </div>
 </div>
 
-<div class="social-icon">
-  <ul>    
-      <li><i class="fa fa-envelope" aria-hidden="true"></i></li>
-      <li><i class="fa fa-facebook" aria-hidden="true"></i></li>
-      <li><i class="fa fa-instagram" aria-hidden="true"></i></li>
-      <li><i class="fa fa-whatsapp" aria-hidden="true"></i></li>              
-  </ul>    
-</div>
+<div class="row1">    
+    <div class="col-a">
+    <h3>Follow</h3>
+      <i class="fa fa-envelope" aria-hidden="true"></i>
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+      <i class="fa fa-instagram" aria-hidden="true"></i>
+      <i class="fa fa-whatsapp" aria-hidden="true"></i>
+    </div> 
+    <div class="col-b">
+      <h3>NoteScout</h3>
+      <img src="123.png" alt="logo">
+    </div>
+    <div class="col-c">
+      <h3>MORE</h3>
+      <a href="#Contact us">Contact</a>
+      <a href="#about us">About us</a>
+      <a href="#help">Help</a>
+      <a href="#faq">FAQ</a>
+    </div>
+
 
 <!--__________________________________________-->
 <script>
@@ -86,6 +91,7 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
 // Get the modal
 var modal = document.getElementById('login');
 
@@ -100,7 +106,9 @@ if (logerr!=null)
 {
 	alert(logerr);
 }
+
 </script>
 
 </body>
 </html>
+
